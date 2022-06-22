0f6e0d46ff1441f2311e19479adfe9ecc795ed20
Johneryy <johnakintolu4@gmail.com>
2022-06-22 16:08:48 +0800

    Python

---
+++ Banjo.py
@@ -0,0 +1,4 @@
+def are_you_playing_banjo(name):
+    return name + " plays banjo" if name[0].lower() == 'r' else name + " does not play banjo"
+Name = input("Enter your name-> ")
+print(are_you_playing_banjo(Name))

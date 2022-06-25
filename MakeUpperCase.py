5c2f940a6f6b69b2143c127aae8419d5452343ca
Johneryy <johnakintolu4@gmail.com>
2022-06-25 16:43:28 +0800

    Python

--- MakeUpperCase.py
+++ MakeUpperCase.py
@@ -1,4 +1,5 @@
-def make_upper_case(string):
-    return string
-str = input("Enter a word-> ").upper()
-print(make_upper_case(str))
+# def make_upper_case(string):
+#     return string
+# str = input("Enter a word-> ").upper()
+# print(make_upper_case(str))
+

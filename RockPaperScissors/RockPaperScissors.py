77e5fb7b051103c147813f9e3d31cd8a43d06f78
Johneryy <johnakintolu4@gmail.com>
2022-06-25 19:17:24 +0800

    Python

--- RockPaperScissors/RockPaperScissors.py
+++ RockPaperScissors/RockPaperScissors.py
@@ -11,37 +11,37 @@ while True:
 
     if player == computer:
         print("computer: ", computer)
-        print("player: ", player)
+        print(name, ":", player)
         print("Tie!")
     elif player == "rock":
         if computer == "paper":
             print("computer: ", computer)
-            print("player: ", player)
+            print(name, ":", player)
             print("You lose!")
         if computer == "scissors":
             print("computer: ", computer)
-            print("player: ", player)
+            print(name, ":", player)
             print("You win!")
     elif player == "scissors":
         if computer == "rock":
             print("computer: ", computer)
-            print("player: ", player)
+            print(name, ":", player)
             print("You lose!")
         if computer == "paper":
             print("computer: ", computer)
-            print("player: ", player)
+            print(name, ":", player)
             print("You win!")
     elif player == "paper":
         if computer == "scissors":
             print("computer: ", computer)
-            print("player: ", player)
+            print(name, ":", player)
             print("You lose!")
         if computer == "rock":
             print("computer: ", computer)
-            print("player: ", player)
+            print(name, ":", player)
             print("You win!")
 
     play_again = input("Play again? (yes/no): ").lower()
     if play_again != "yes":
         break
-print("Bye!",name)
+print("Bye", name)
